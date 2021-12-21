d94d01eceff56a2fff74fc68313ba245c1a7aeec
ibrahimElmourchidi <ibrahimelmourchidi2@gmail.com>
2021-12-21 09:28:18 +0200

    deploy setting updated and add new fonts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,19 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './en/landing/header/header.component';
 import { MainComponent } from './en/landing/main/main.component';
 import { FooterComponent } from './en/landing/footer/footer.component';
+import { FormContainerComponent } from './en/auth/form-container/form-container.component';
+import { LoginComponent } from './en/auth/login/login.component';
+import { SignupComponent } from './en/auth/signup/signup.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     MainComponent,
-    FooterComponent
+    FooterComponent,
+    FormContainerComponent,
+    LoginComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,

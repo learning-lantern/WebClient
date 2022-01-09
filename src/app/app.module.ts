60a5ee31d01d00a3a456e8e73d3b4078402ac022
ibrahimElmourchidi <ibrahimelmourchidi2@gmail.com>
2022-01-09 09:27:33 +0200

    try

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-
-//this is to try the hard mergegit reset --hard upstream/branch1
-
-
 @NgModule({
   declarations: [
     AppComponent,

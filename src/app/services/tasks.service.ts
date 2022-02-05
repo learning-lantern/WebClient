39e662f37429c404f5e8a24c995fc1cf3218c22a
IbrahimElmourchidi <ibrahimelmourchidi2@gmail.com>
2022-02-05 07:02:49 +0200

    new colors

--- src/app/services/tasks.service.ts
+++ src/app/services/tasks.service.ts
@@ -1,19 +1,21 @@
 import { Injectable } from '@angular/core';
-import { Task } from "../languages/english/interfaces/task";
+import { TaskInterface } from '../languages/english/interfaces/task';
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class TasksService {
-  tasksList: Task[] = []
-  constructor() { }
+  tasksList: TaskInterface[] = [];
+  constructor() {}
   saveTask(title: string, discription: string) {
-    console.log(title,discription)
     this.tasksList.push({
       title,
-      discription
-    })
-  };
+      discription,
+    });
+  }
   deleteTask(index: number) {
-    this.tasksList.splice(index, 1)
-  };
+    this.tasksList.splice(index, 1);
+  }
+  updateTask(index: number, task: any) {
+    this.tasksList[index] = task;
+  }
 }

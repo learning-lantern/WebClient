d84fd6e316646c6c5986f6cd9120592828501e07
IbrahimElmourchidi <ibrahimelmourchidi2@gmail.com>
2022-02-09 13:24:59 +0200

    landing english done

--- src/app/languages/arabic/landing/header/header.component.ts
+++ src/app/languages/arabic/landing/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -7,13 +8,30 @@ import { Component } from '@angular/core';
 })
 export class HeaderComponent {
   isActive = 'home';
+  drop = false;
   isHidden = true;
-  constructor() {}
+  constructor(private router: Router) {}
 
   changeActive(str: string) {
     this.isActive = str;
   }
+
   showSidebar() {
     this.isHidden = !this.isHidden;
+  }
+
+  dropToggle() {
+    this.drop = !this.drop;
+  }
+
+  switchLang(oldLang: string, newLang: string) {
+    const url = this.router.url;
+    const url2 = url.replace(`/${oldLang}`, `/${newLang}`);
+    this.router.navigate([url2]);
+  }
+  scrollTO(str: string) {
+    document
+      .getElementById(str)
+      ?.scrollIntoView({ behavior: 'smooth', block: 'start' });
   }
 }
